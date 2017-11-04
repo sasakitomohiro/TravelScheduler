3c19ab95358dabb2294b008e4619e6d44f6c8a6e
TomohiroSasaki <t0m0.piii.12.07sk@gmail.com>
2017-11-04 16:02:36 +0900

    first

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+Vue.component("common-menu", {
+  props: ["current"],
+  template: `
+    <div class="ui secondary pointing menu">
+      <a class="item" href="./index.html" v-bind:class="{active: current=='index'}">
+      トップ
+      </a>
+    </div>`,
+methods: {
+}
+});
